7e65758d5177252fcd21643d1d52bdd255a5a5f2
liukunhao <954001442@qq.com>
2020-12-03 22:19:31 +0800

    SymbolTable Bug fix

--- src/main/java/miniplc0java/SymbolTable/SymbolTable.java
+++ src/main/java/miniplc0java/SymbolTable/SymbolTable.java
@@ -9,8 +9,12 @@ import java.util.List;
 public class SymbolTable {
     List<Symbol> symbolList = new ArrayList<>();
     private int argCount = 1; // arg0 被返回值占用
-    private int varCount = 0;
     private int funcCount = 1; // func0 被_start占用
+
+    private int localCount = 0;
+    private int globalCount = 0;
+
+    // TODO 嵌套
 
     public void addSymbol (Symbol symbol) throws AnalyzeError {
 
@@ -24,16 +28,23 @@ public class SymbolTable {
         }
 
         // 设置序号，更新count
-        if (symbol.kind.equals("arg")) {
+        if (symbol.kind.equals("func")) {
+            symbol.num = funcCount;
+            funcCount++;
+        } else if (symbol.kind.equals("arg")) {
             symbol.num = argCount;
             argCount++;
         } else if (symbol.kind.equals("var")) {
-            symbol.num = varCount;
-            varCount++;
-        } else if (symbol.kind.equals("func")) {
-            symbol.num = funcCount;
-            funcCount++;
-        }
+            if (symbol.level == -1) {
+                // 全局变量
+                symbol.num = globalCount;
+                globalCount++;
+            } else {
+                // 局部变量
+                symbol.num = localCount;
+                localCount++;
+            }
+        } else throw new Error("程序写错了，Symbol的kind只能是func/arg/var");
 
         symbolList.add(symbol);
     }
@@ -46,7 +57,7 @@ public class SymbolTable {
         return null;
     }
 
-    // 找变量和参数，一层层往上找
+    // 找变量和参数，一层层往下找
     public Symbol searchVarArgSymbol (String name, int level) {
         while (level>=-1) {
             for (Symbol s: symbolList) {
@@ -60,12 +71,15 @@ public class SymbolTable {
     // 一个块结束的时候调用
     public void deleteSymbolOfLevel (int level) {
 
-        // 删除除了全局变量和函数外的所有东西，即要删除arg
+        // 删除除了全局变量和函数外的所有东西，即要删除arg，重置argCount/localCount
         if (level == 0) {
             symbolList.removeIf(s -> s.level != -1);
+            argCount = 1;
+            localCount = 0;
         } else if (level > 0) {
             // 删除当前层的var
             symbolList.removeIf(s -> s.level == level);
         }
+
     }
 }
